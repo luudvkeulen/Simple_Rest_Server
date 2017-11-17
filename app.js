97bf72873cadff08f8476664c16dfac98b28bc7c
Luud van Keulen <luudvkeulen@gmail.com>
2017-11-17 10:57:55 +0100

    Prefixed with /api to fix nginx reverse proxy.

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => res.send("Hello world"));
+app.get('/api/', (req, res) => res.send("Hello world"));
 
-app.get('/test', (req, res) => res.send("Test ok"));
+app.get('/api/test', (req, res) => res.send("Test ok"));
 
 app.listen(port, () => console.log("Server started on ", port));
