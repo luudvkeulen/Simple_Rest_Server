f833bb3a6067a4f33a78a06341c8e98a08a93cd4
Luud van Keulen <luudvkeulen@gmail.com>
2017-11-17 10:15:28 +0100

    Added get requests.

--- app.js
+++ app.js
@@ -2,4 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.listen(port, () => console.log("Server started on "));
+app.get('/', (req, res) => res.send("Hello world"));
+
+app.get('/test', (req, res) => res.send("Test ok"));
+
+app.listen(port, () => console.log("Server started on ", port));
